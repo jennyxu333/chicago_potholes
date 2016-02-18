776de8d7e0f37d7695b7f86664af45239051097f
Jenny Xu <jennyxu333@cs.uchicago.edu>
2016-02-17 22:18:18 -0600

    :P

--- api_testing.py
+++ api_testing.py
@@ -1,10 +1,7 @@
 import csv
+import json
 
-ph = open('potholes_reported.csv', 'r')
-fieldnames = ('creation_date', 'status', 'completion_date', 'service_request_number',\
-                'type_of_service_request', 'current_activity', 'most_recent_action', \
-                'number_of_potholes_filled_on_block', 'street_address', 'zip', \
-                'x_coordinate', 'y_coordinate', 'ward', 'police_district', \
-                'community_area', 'ssa', 'latitude', 'longtitude', 'location', \
-                'location_zip', 'location_city', 'location_address', 'location_state')
-reader = csv.DictReader(ph, fieldnames)
+csvfile = open('potholes_reported.csv', 'r')
+jsonfile = open('potholes_reported.json', 'w')
+
+fieldnames = 
